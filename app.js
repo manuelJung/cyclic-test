01d39576e95965b82fce51643e615848a7b97937
manuelJung <manuel.jung.wwi12@gmail.com>
2022-10-13 10:30:50 +0200

    added bcrypt and crypto

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
 
 const PORT = 3000
 
 const app = express()
 
-app.use((req, res) => {
-  res.status(200).send('hello world')
+app.use(async (req, res) => {
+  const pw = await bcrypt.hash('foo', 10)
+  const equal = await bcrypt.compare('foo', pw)
+  const token = crypto.randomBytes(64).toString('hex')
+
+  res.status(200).send(`hello world. equal: ${equal}, token:${token}`)
 })
 
 app.listen(PORT, () => {
