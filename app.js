a3c3ddf3501b8211916dd1591f88c48944f46422
manuelJung <manuel.jung.wwi12@gmail.com>
2022-10-13 11:09:24 +0200

    updated

--- app.js
+++ app.js
@@ -11,8 +11,21 @@ app.use(async (req, res) => {
   const pw = await bcrypt.hash('foo', 10)
   const equal = await bcrypt.compare('foo', pw)
   const token = crypto.randomBytes(64).toString('hex')
-  fs.writeFileSync(__dirname + '/test.txt', 'Helllo World!', 'utf-8')
-  const data = fs.readFileSync(__dirname + '/test.txt', 'utf-8')
+  const filename = __dirname + '/test.txt'
+  try {
+    fs.unlinkSync(filename)
+  }
+  catch(e) {}
+  fs.closeSync(fs.openSync(filename, "w"))
+  // const fd = fs.openSync(filename, "r+")
+  // const text = "Welcome to Test"
+  // const position = 0
+  // fs.writeSync(fd, text, position, 'utf8')
+  // fs.closeSync()
+
+
+  // fs.writeFileSync(__dirname + '/test.txt', 'Helllo World!', 'utf-8')
+  const data = fs.readFileSync(filename, 'utf-8')
 
   res.status(200).send(`${data} equal: ${equal}, token:${token}`)
 })
