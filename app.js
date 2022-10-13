c07f3abf0ff0fcf59cca8e4cd7ea55f70ca85b4e
manuelJung <manuel.jung.wwi12@gmail.com>
2022-10-13 11:30:29 +0200

    added mongodb

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const DB_PORT = 27017
 const DB_NAME = 'devq'
 const PORT = 3000
 
-mongoose.connect(`mongodb://${DB_URL}:${DB_PORT}/${DB_NAME}`).then(() => console.log('mongo started'))
+const uri = 'mongodb+srv://mjung:mongotest@cluster0.di05mk9.mongodb.net/?retryWrites=true&w=majority';
+const client = new mongoose.MongoClient(uri)
 
 
 const app = express()
@@ -22,9 +23,17 @@ app.use(async (req, res) => {
   fs.writeFileSync(filename, 'Helllo World!', 'utf-8')
   const data = fs.readFileSync(filename, 'utf-8')
 
-  res.status(200).send(`${data}. equal: ${equal}, token:${token}`)
+  let item = await client.db("my_db")
+                .collection("my_collection")
+                .findOne({my_item: my_item})
+
+  res.status(200).send(`${data}. equal: ${equal}, token:${token}, item: ${item}`)
 })
 
-app.listen(PORT, () => {
-  console.log('Server running on port', PORT)
-})
+client.connect(err => {
+  if(err){ console.error(err); return false;}
+  // connection to mongo is successful, listen for requests
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+});
